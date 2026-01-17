07210395df730949eed91cd9c3ecc0d5d1d376bc
Ujk768 <utkarsh.kanade1@gmail.com>
2026-01-17 18:39:57 -0500

    Added bounce animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,63 @@
+"use client";
+
+import { useTheme } from "next-themes";
 import Image from "next/image";
+import { motion } from "motion/react";
 
 export default function Home() {
+  const { theme } = useTheme();
   return (
-    <div className="bg-[var(--background)] text-[var(--foreground)] shadow-lg relative border-b border-gray-200 dark:border-gray-800 transition-colors duration-300">
-      <main className="flex flex-col p-5 w-full h-full">
-        <div>
-          <h1>About Me</h1>
+    <div
+      className={`bg-background relative transition-colors duration-300 
+      `}
+    >
+      <main className="w-full h-full">
+        <div
+          className={`w-full h-svh justify-center p-5 items-center align-center ${theme === "dark" ? "bg-gray-800" : "bg-white"}`}
+        >
+          <div className="flex md:flex-row flex-col justify-center items-center gap-10">
+            <div className="w-1/4 flex justify-center items-center">
+              <div className="animate-[bounce_1s_ease-in-out_2]">
+                {theme === "dark" ? (
+                  <Image
+                    className="rounded-full aspect-square object-cover"
+                    src="/kakashi-dark.jpg"
+                    alt="Kakashi"
+                    width={400}
+                    height={400}
+                  />
+                ) : (
+                  <Image
+                    className="rounded-full aspect-square object-cover"
+                    src="/kakashi-light.webp"
+                    alt="Kakashi"
+                    width={400}
+                    height={400}
+                  />
+                )}
+              </div>
+            </div>
+            <div className="w-3/4 justify-center items-center align-center">
+              <h1 className="font-bold text-center text-4xl p-10">
+                Hi, I'm Utkarsh Kanade
+              </h1>
+              <h6 className="font-medium text-center text-2xl ">
+                I’m a Full-Stack Developer with a strong focus on React and
+                Next.js, passionate about building performant, scalable, and
+                user-centric web applications. I enjoy working across the stack,
+                learning system design, and turning ideas into reliable
+                products.
+              </h6>
+            </div>
+          </div>
         </div>
         <div>
+          <h1>Experience</h1>
+          <h1>Experience</h1>
+          <h1>Experience</h1>
+          <h1>Experience</h1>
+          <h1>Experience</h1>
+          <h1>Experience</h1>
           <h1>Experience</h1>
         </div>
         <div>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,18 +9,21 @@ const scrollToTop = () => {
 };
 
 export default function NavBar() {
-  const [mounted, setMounted] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const { theme, setTheme } = useTheme();
 
-  // useEffect ensures the component is mounted before we show the UI
-  // This prevents hydration errors with icons
-  useEffect(() => setMounted(true), []);
-
-  if (!mounted) return null;
+  // fix hydration error
+  useEffect(() => {
+    setIsOpen(false);
+  });
 
   return (
-    <nav className="bg-[var(--background)] text-[var(--foreground)] shadow-lg relative border-b border-gray-200 dark:border-gray-800 transition-colors duration-300">
+    <nav
+      className={` text-foreground shadow-lg relative transition-colors duration-300 
+        border-b-2 border-gray-800
+        ${theme === "dark" ? "bg-gray-800" : "bg-white"}
+      `}
+    >
       <div className="flex items-center justify-between px-6 md:px-10 py-4">
         <div className="flex-shrink-0">
           <Image
@@ -38,26 +41,36 @@ export default function NavBar() {
         <div className="md:hidden flex space-x-5">
           <div>
             <button onClick={() => setIsOpen(!isOpen)} className="p-2">
-              {isOpen ? <X size={24} /> : <Menu size={24} />}
+              {isOpen ? (
+                <X
+                  size={24}
+                  color={`${theme === "dark" ? "white" : "black"}`}
+                />
+              ) : (
+                <Menu
+                  size={24}
+                  color={`${theme === "dark" ? "white" : "black"}`}
+                />
+              )}
             </button>
           </div>
 
           <div className="flex items-center flex-col">
             <button
               onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-              className="p-2 rounded-lg bg-white-500 dark:bg-gray-800 transition-all cursor-pointer"
+              className="p-2 rounded-lg transition-all cursor-pointer"
             >
               {theme === "dark" ? (
                 <Sun size={20} color="yellow" />
               ) : (
-                <Moon size={20} color="white" />
+                <Moon size={20} color="black" />
               )}
             </button>
           </div>
         </div>
 
         {/* Desktop Links (Hidden on Mobile) */}
-        <div className="hidden md:flex md:space-x-10 lg:space-x-20 font-medium text-lg cursor-pointer">
+        <div className="hidden md:flex md:space-x-10 lg:space-x-20 text-lg cursor-pointer font-bold">
           <div className="hover:underline">About Me</div>
           <div className="hover:underline">Experience</div>
           <div className="hover:underline">Projects</div>
@@ -79,7 +92,7 @@ export default function NavBar() {
       </div>
       {/* Mobile Menu Dropdown */}
       {isOpen && (
-        <div className="md:hidden absolute top-full left-0 w-full bg-[var(--background)] text-[var(--foreground)] flex flex-col items-center space-y-4 py-6 font-medium shadow-xl z-50">
+        <div className="md:hidden absolute top-full left-0 w-full bg-background text-foreground flex flex-col items-center space-y-4 py-6 font-medium shadow-xl z-50">
           <div onClick={() => setIsOpen(false)}>About Me</div>
           <div onClick={() => setIsOpen(false)}>Experience</div>
           <div onClick={() => setIsOpen(false)}>Projects</div>
